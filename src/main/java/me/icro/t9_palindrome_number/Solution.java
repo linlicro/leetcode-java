cdb344eca509b2312e747b3eb0c407eddd40c648
Lin Li <lilin@treefinance.com.cn>
2019-04-02 21:51:39 +0800

    add: t9

---
+++ src/main/java/me/icro/t9_palindrome_number/Solution.java
@@ -0,0 +1,17 @@
+package me.icro.t9_palindrome_number;
+
+/**
+ * 描述:
+ * 9. Palindrome Number
+ * https://leetcode.com/problems/palindrome-number/
+ *
+ * @author Lin
+ * @since 2019-04-02 4:02 PM
+ */
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+        return true;
+    }
+}
