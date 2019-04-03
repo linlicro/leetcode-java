5bdf2bfb511a1394b7b4b8805f7543b70704f0b0
Lin Li <lilin@treefinance.com.cn>
2019-04-03 16:32:55 +0800

    add: t9

--- src/main/java/me/icro/t9_palindrome_number/Solution.java
+++ src/main/java/me/icro/t9_palindrome_number/Solution.java
@@ -9,9 +9,19 @@ package me.icro.t9_palindrome_number;
  * @since 2019-04-02 4:02 PM
  */
 public class Solution {
-    public boolean isPalindrome(int x) {
+    public boolean isPalindrome(int x) { // convert the number into string, and check if the string is a palindrome
         if (x < 0) return false;
-
+        if (x < 10) return true;
+        String numString = String.valueOf(x);
+        for (int i = 0; i < numString.length() / 2; i++) {
+            if (numString.charAt(numString.length() - i - 1) != numString.charAt(i)) {
+                return false;
+            }
+        }
         return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().isPalindrome(121));
     }
 }
