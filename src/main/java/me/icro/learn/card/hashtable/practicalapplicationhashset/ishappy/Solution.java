1ec65913bb042d1b831d2e146365084603d803fa
linlicro <linli.cro@gmail.com>
2019-12-05 17:44:49 +0800

    feat: happy number

--- src/main/java/me/icro/learn/card/hashtable/practicalapplicationhashset/ishappy/Solution.java
+++ src/main/java/me/icro/learn/card/hashtable/practicalapplicationhashset/ishappy/Solution.java
@@ -1,5 +1,8 @@
 package me.icro.learn.card.hashtable.practicalapplicationhashset.ishappy;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 描述: https://leetcode-cn.com/explore/learn/card/hash-table/204/practical-application-hash-set/808/
  *
@@ -8,7 +11,37 @@ package me.icro.learn.card.hashtable.practicalapplicationhashset.ishappy;
  */
 public class Solution {
     public boolean isHappy(int n) {
+        if (1 == n) {
+            return true;
+        }
+
+        Set<Integer> sadNumbers = new HashSet<>();
+        sadNumbers.add(n);
+
+        int nextNum = powerEachDigital(n);
+        while (1 != nextNum
+                && !sadNumbers.contains(nextNum)) {
+            sadNumbers.add(nextNum);
+            nextNum = powerEachDigital(nextNum);
+        }
+
+        if (1 == nextNum)
+            return true;
 
         return false;
+    }
+
+    private int powerEachDigital(int n) {
+        int res = 0;
+        while (n != 0) {
+            res += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().isHappy(20));
+        System.out.println(new Solution().isHappy(18));
     }
 }
