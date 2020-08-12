3134edffcf8835e8ef4d2a2594c3b8ec8f15e29d
Lin <linli.cro@gmail.com>
2020-08-12 16:42:06 +0800

    t 235

---
+++ src/main/java/me/icro/problems/t235lowestcommonancestor/Solution.java
@@ -0,0 +1,16 @@
+package me.icro.problems.t235lowestcommonancestor;
+
+/**
+ * @author lin
+ * @version v 0.1 2020/8/12
+ **/
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+}
