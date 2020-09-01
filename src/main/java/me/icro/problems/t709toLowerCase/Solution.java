494bdff2b0066214133a2435c091dd3024c59556
Lin <linli.cro@gmail.com>
2020-09-01 09:08:37 +0800

    t 709

---
+++ src/main/java/me/icro/problems/t709toLowerCase/Solution.java
@@ -0,0 +1,14 @@
+package me.icro.problems.t709toLowerCase;
+
+/**
+ * @author lin
+ * @version v 0.1 2020/9/1
+ **/
+public class Solution {
+    public String toLowerCase(String str) {
+        if (null == str) {
+            return null;
+        }
+        return str.toLowerCase();
+    }
+}
